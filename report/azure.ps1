98a1cf57d257e3c392c812ed1802a793f5bb8032
Aditya Kumar <59906496+theadityakr@users.noreply.github.com>
2023-12-05 04:28:57 -0500

    Update azure.ps1

--- report/azure.ps1
+++ report/azure.ps1
@@ -7,11 +7,11 @@ foreach ($x in $jsonFiles) {
 $file = $x.FullName
 $name =  (Get-Item $file).name
 
-$HTTPSStorageHost = "https://prometheuschat.blob.core.windows.net"
+$HTTPSStorageHost = "https://dfdffdffdf"
 
 $Container = "salt"
 
-$SASToken = "?sp=racwdli&st=2023-12-03T15:30:46Z&se=2023-12-05T15:30:46Z&spr=https&sv=2022-11-02&sr=c&sig=wdFSWFcF5mE4bAqmFfRxFmfgPLYNI3LNlt6jWTHGRvM%3D"
+$SASToken = "?dfvdfdff"
 
 $URI = "$($HTTPSStorageHost)/$($Container)/$($name)$($SASToken)"
 
